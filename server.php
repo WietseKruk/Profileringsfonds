08e8691a15050e2459d0ce18c4116c9a795455aa
Patrick <Petjenorden@gmail.com>
2020-04-29 12:41:07 +0200

    Spaghetti is gone

--- server.php
+++ server.php
@@ -204,129 +204,6 @@ if (isset($_POST['change_pass'])) {
 
 if (isset($_POST['submit_form'])) {
 
-
-    $snr = $_POST['studentnummer'];
-    $l_name = $_POST['achternaam'];
-    $f_name = $_POST['roepnaam'];
-    $dob = $_POST['geboortedatum'];
-    $address = $_POST['adres'];
-    $postcode = $_POST['postcode'];
-    $wp = $_POST['woonplaats'];
-    $tnummer = $_POST['tnummer'];
-    $mail = $_POST['email'];
-    $bsn = $_POST['bsn'];
-    $iban = $_POST['iban'];
-    $inschr = $_POST['ingeschreven'];
-    $opleiding = $_POST['opleiding'];
-    $variant = $_POST['variant'];
-    $gestart = $_POST['gestart'];
-    $jaar = $_POST['jaar'];
-    $onderbroken = $_POST['onderbroken'];
-    $uitschr = $_POST['uitgeschreven'];
-    $andere = $_POST['andere'];
-    $instel = $_POST['instelling'];
-    $aanl = $_POST['aanleiding'];
-    $maand = $_POST['maanden'];
-    $duo = $_POST['duo'];
-    $extra = $_POST['extra'];
-    $ingeg = $_POST['ingegaan'];
-    $finance = $_POST['financieel'];
-    $eerder = $_POST['eerder'];
-    $waaruit = $_POST['waaruit'];
-    $eindig = $_POST['eindig'];
-    $melding = $_POST['melding'];
-    $afmeld = $_POST['afmeld'];
-    $studieond = $_POST['studieond'];
-    $duur = $_POST['duur'];
-    $verloop = $_POST['verloop'];
-    $wijze = $_POST['wijze'];
-
-    if (empty($snr)) {
-        array_push($errors, "Je hebt veld 1 niet ingevuld");
-    }
-    if (empty($l_name)) {
-        array_push($errors, "Je hebt veld 2 niet ingevuld");
-    }
-    if (empty($f_name)) {
-        array_push($errors, "Je hebt veld 3 niet ingevuld");
-    }
-    if (empty($dob)) {
-        array_push($errors, "Je hebt veld 4 niet ingevuld");
-    }
-    if (empty($address)) {
-        array_push($errors, "Je hebt veld 5 niet ingevuld");
-    }
-    if (empty($postcode)) {
-        array_push($errors, "Je hebt veld 6 niet ingevuld");
-    }
-    if (empty($wp)) {
-        array_push($errors, "Je hebt veld 7 niet ingevuld");
-    }
-    if (empty($tnummer)) {
-        array_push($errors, "Je hebt veld 8 niet ingevuld");
-    }
-    if (empty($mail)) {
-        array_push($errors, "Je hebt veld 9 niet ingevuld");
-    }
-    if (empty($bsn)) {
-        array_push($errors, "Je hebt veld 10 niet ingevuld");
-    }
-    if (empty($iban)) {
-        array_push($errors, "Je hebt veld 11 niet ingevuld");
-    }
-    if (empty($opleiding)) {
-        array_push($errors, "Je hebt veld 13 niet ingevuld");
-    }
-    if (empty($gestart)) {
-        array_push($errors, "Je hebt veld 15 niet ingevuld");
-    }
-    if (empty($andere)) {
-        array_push($errors, "Je hebt veld 19 niet ingevuld");
-    }
-    if (empty($instel)) {
-        array_push($errors, "Je hebt veld 20 niet ingevuld");
-    }
-    if (empty($maand)) {
-        array_push($errors, "Je hebt veld 22 niet ingevuld");
-    }
-    if (empty($ingeg)) {
-        array_push($errors, "Je hebt veld 25 niet ingevuld");
-    }
-    if (empty($finance)) {
-        array_push($errors, "Je hebt veld 26 niet ingevuld");
-    }
-    if (empty($eerder)) {
-        array_push($errors, "Je hebt veld 27 niet ingevuld");
-    }
-    if (empty($waaruit)) {
-        array_push($errors, "Je hebt veld 28 niet ingevuld");
-    }
-    if (empty($eindig)) {
-        array_push($errors, "Je hebt veld 29 niet ingevuld");
-    }
-    if (empty($melding)) {
-        array_push($errors, "Je hebt veld 30 niet ingevuld");
-    }
-    if (empty($afmeld)) {
-        array_push($errors, "Je hebt veld 31 niet ingevuld");
-    }
-    if (empty($studieond)) {
-        array_push($errors, "Je hebt veld 32 niet ingevuld");
-    }
-    if (empty($duur)) {
-        array_push($errors, "Je hebt veld 33 niet ingevuld");
-    }
-    if (empty($verloop)) {
-        array_push($errors, "Je hebt veld 34 niet ingevuld");
-    }
-    if (empty($wijze)) {
-        array_push($errors, "Je hebt veld 35 niet ingevuld");
-    }
-
-//    $f_name = $_POST[''];
-//    $f_name = $_POST[''];
-//    $f_name = $_POST[''];
-
     if (count($errors) == 0) {
 
         require('C:\wamp64\bin\apache\apache2.4.41\htdocs\fpdf.php');
@@ -337,121 +214,17 @@ if (isset($_POST['submit_form'])) {
 
         $pdf->SetFont("Arial", "B", 5);
 
-        $pdf->Cell(140, 10, "$f_name's Persoonsgegevens", 1, 1, 'C');
-
-        $pdf->Cell(65, 10, "Studentnummer: ", 1, 0);
-        $pdf->Cell(75, 10, $snr, 1, 1);
-
-        $pdf->Cell(65, 10, "Achternaam: ", 1, 0);
-        $pdf->Cell(75, 10, $l_name, 1, 1);
-
-        $pdf->Cell(65, 10, "Roepnaam: ", 1, 0);
-        $pdf->Cell(75, 10, $f_name, 1, 1);
-
-        $pdf->Cell(65, 10, "Geboortedatum: ", 1, 0);
-        $pdf->Cell(75, 10, $dob, 1, 1);
-
-        $pdf->Cell(65, 10, "Adres: ", 1, 0);
-        $pdf->Cell(75, 10, $address, 1, 1);
-
-        $pdf->Cell(65, 10, "Postcode: ", 1, 0);
-        $pdf->Cell(75, 10, $postcode, 1, 1);
-
-        $pdf->Cell(65, 10, "Woonplaats: ", 1, 0);
-        $pdf->Cell(75, 10, $wp, 1, 1);
-
-        $pdf->Cell(65, 10, "Telefoonnummer: ", 1, 0);
-        $pdf->Cell(75, 10, $tnummer, 1, 1);
-
-        $pdf->Cell(65, 10, "E-mailadres: ", 1, 0);
-        $pdf->Cell(75, 10, $mail, 1, 1);
-
-        $pdf->Cell(65, 10, "BSN-nummer: ", 1, 0);
-        $pdf->Cell(75, 10, $bsn, 1, 1);
-
-        $pdf->Cell(65, 10, "Iban Rekeningnummer: ", 1, 0);
-        $pdf->Cell(75, 10, $iban, 1, 1);
-
-        $pdf->AddPage();
-
-        $pdf->SetFont("Arial", "B", 5);
-
-        $pdf->Cell(140, 10, "$f_name's Antwoorden", 1, 1, 'C');
-
-        $pdf->Cell(65, 10, "Vraag 1", 1, 0);
-        $pdf->Cell(75, 10, $inschr, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 2", 1, 0);
-        $pdf->Cell(75, 10, $opleiding, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 3", 1, 0);
-        $pdf->Cell(75, 10, $variant, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 4", 1, 0);
-        $pdf->Cell(75, 10, $gestart, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 5", 1, 0);
-        $pdf->Cell(75, 10, $jaar, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 6", 1, 0);
-        $pdf->Cell(75, 10, $onderbroken, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 7", 1, 0);
-        $pdf->Cell(75, 10, $uitschr, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 8", 1, 0);
-        $pdf->Cell(75, 10, $andere, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 9", 1, 0);
-        $pdf->Cell(75, 10, $instel, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 10", 1, 0);
-        $pdf->Cell(75, 10, $andere, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 11", 1, 0);
-        $pdf->Cell(75, 10, $aanl, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 12", 1, 0);
-        $pdf->Cell(75, 10, $maand, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 13", 1, 0);
-        $pdf->Cell(75, 10, $duo, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 14", 1, 0);
-        $pdf->Cell(75, 10, $extra, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 15", 1, 0);
-        $pdf->Cell(75, 10, $ingeg, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 16", 1, 0);
-        $pdf->Cell(75, 10, $finance, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 17", 1, 0);
-        $pdf->Cell(75, 10, $eerder, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 18", 1, 0);
-        $pdf->Cell(75, 10, $waaruit, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 19", 1, 0);
-        $pdf->Cell(75, 10, $eindig, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 20", 1, 0);
-        $pdf->Cell(75, 10, $melding, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 21", 1, 0);
-        $pdf->Cell(75, 10, $afmeld, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 22", 1, 0);
-        $pdf->Cell(75, 10, $studieond, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 23", 1, 0);
-        $pdf->Cell(75, 10, $duur, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 24", 1, 0);
-        $pdf->Cell(75, 10, $verloop, 1, 1);
-
-        $pdf->Cell(65, 10, "Vraag 25", 1, 0);
-        $pdf->Cell(75, 10, $wijze, 1, 1);
+        $pdf->Cell(140, 10, "Persoonsgegevens", 1, 1, 'C');
+        foreach ($_POST as $key => $value) {
+            $value = mysqli_real_escape_string($conn, $value);
+            $value = addslashes($value);
+            $value = strip_tags($value);
+            if (empty($value)) {
+                array_push($errors, "je moeder");
+            }
+            $pdf->Cell(65, 10, $key, 1, 0);
+            $pdf->Cell(75, 10, $value, 1, 1);
+        }
 
 
         $id = $_SESSION['id'];
@@ -459,7 +232,7 @@ if (isset($_POST['submit_form'])) {
         $fullpath = "C:/wamp64/www/periode3/Selecta/Profileringsfonds/public_html/" . $id . ".pdf";
         $dbpath = "/periode3/Selecta/Profileringsfonds/public_html/" . $id . ".pdf";
 
-        if (!empty($id)) {
+        if (!empty($id) && count($errors) == 0) {
             $pdf->Output("$fullpath", "F");
             $query = "INSERT INTO formulier (path, uID, uName) VALUES('$dbpath', '$id', '$user')";
             mysqli_query($conn, $query);
@@ -560,28 +333,3 @@ else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_
 }
 
 require_once "languages/" . $_SESSION['lang'] . ".php";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Tabel alles is not null van formulier tabel
